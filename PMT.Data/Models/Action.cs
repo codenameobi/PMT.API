4069d0f33c2599224324daa179027b23ac3fb140
codenameobi <tobynwude@gmail.com>
2023-11-23 23:43:49 +0000

    Add Action Model

---
+++ PMT.Data/Models/Action.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMT.Data.Models
+{
+    internal class Action
+    {
+        public int TaskId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Project project { get; set;}
+    }
+}
---
+++ PMT.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PMT.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+           
+        }
+    }
+}
